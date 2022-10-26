9310a6f47a2c4132fc1ec67debb52ca05d72cb6c
new_user <nrustgen@gmail.com>
2022-10-26 02:49:54 -0700

    0. Spring MVC [#6877]

---
+++ src/main/java/ru/job4j/accidents/Main.java
@@ -0,0 +1,18 @@
+package ru.job4j.accidents;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+        System.out.println("Go to http://localhost:8080/index");
+    }
+}
