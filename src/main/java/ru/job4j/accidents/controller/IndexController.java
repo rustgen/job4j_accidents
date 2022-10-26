9b9eb92457331d87b4e2b1abeef8a32590e7231a
new_user <nrustgen@gmail.com>
2022-10-26 04:04:23 -0700

    1. Loading data into the view [#295782]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,13 +1,15 @@
 package ru.job4j.accidents.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("user", "new User");
         return "index";
     }
 }
