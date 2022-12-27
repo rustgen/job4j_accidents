ae62a07346f1f0a512f0c30278605f2e3f1a6d7e
new_user <nrustgen@gmail.com>
2022-12-27 14:38:18 -0800

    2. User registration [#296069]

--- src/main/java/ru/job4j/accident/Main.java
+++ src/main/java/ru/job4j/accident/Main.java
@@ -2,6 +2,7 @@ package ru.job4j.accident;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Main {
@@ -9,5 +10,9 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
         System.out.println("Go to http://localhost:8080/index");
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String pwd = encoder.encode("secret");
+        System.out.println(pwd);
     }
 }
